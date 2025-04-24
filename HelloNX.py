0c9dce1ce76a75d4138d0a1baf39da6d9dcb346e
Hung <Hungnguyenvan2907@gmail.com>
2025-04-24 11:19:03 +0700

    second changes

--- HelloNX.py
+++ HelloNX.py
@@ -4,3 +4,5 @@ session = NXOpen.Session.GetSession()
 print("NX đã khởi động thành công!")
 
 C:\Program Files\Siemens\NX\NX1980\UGII\python\python.exe -m venv D:\00.WORK\00.FPT\2025\NX\nx_env
+
+C:\Program Files\Siemens\NX\NX1980\UGII\python\python.exe -m venv D:\00.WORK\00.FPT\2025\NX\nx_env
