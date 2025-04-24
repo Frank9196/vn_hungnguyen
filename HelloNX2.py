27a79956b6cb1fccbcf19523e101e393ff99aaa3
Hung <Hungnguyenvan2907@gmail.com>
2025-04-24 11:19:56 +0700

    third change

--- HelloNX2.py
+++ HelloNX2.py
@@ -8,6 +8,6 @@ def main():
     #theSession.ListingWindow.Open()
     #/theSession.ListingWindow.WriteLine("Xin chào NX")
     theUI.NXMessageBox.Show("Xin Chào", NXOpen.NXMessageBox.DialogType.Information, "Xin chào NX")
-
+    theUI.NXMessageBox.Show("Xin Chào", NXOpen.NXMessageBox.DialogType.Information, "Xin chào NX")
 if __name__ == '__main__':
     main()
