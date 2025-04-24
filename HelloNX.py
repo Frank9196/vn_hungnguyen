5ca140bba271e213d93b376d6053ec80b053fb8f
nguyen van hung <hungnguyenvan2907@gmail.com>
2025-04-24 11:07:27 +0700

    initial commit

---
+++ HelloNX.py
@@ -0,0 +1,6 @@
+import NXOpen
+
+session = NXOpen.Session.GetSession()
+print("NX đã khởi động thành công!")
+
+C:\Program Files\Siemens\NX\NX1980\UGII\python\python.exe -m venv D:\00.WORK\00.FPT\2025\NX\nx_env
---
+++ HelloNX2.py
@@ -0,0 +1,13 @@
+﻿import NXOpen
+
+def main():
+
+    theSession  = NXOpen.Session.GetSession()
+    theUI = NXOpen.UI.GetUI()
+
+    #theSession.ListingWindow.Open()
+    #/theSession.ListingWindow.WriteLine("Xin chào NX")
+    theUI.NXMessageBox.Show("Xin Chào", NXOpen.NXMessageBox.DialogType.Information, "Xin chào NX")
+
+if __name__ == '__main__':
+    main()
